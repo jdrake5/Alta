56209a578f45883f694ec66385d8d7f03142ba21
jdrake5 <jdrake8@gmail.com>
2019-05-12 00:12:18 -0500

    downloaded audio file, did not save

--- app/src/main/java/com/example/wind_turbine_tapes/MainActivity.java
+++ app/src/main/java/com/example/wind_turbine_tapes/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.wind_turbine_tapes;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
@@ -10,15 +14,31 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+
+import es.dmoral.toasty.Toasty;
 
 public class MainActivity extends AppCompatActivity {
     private SlidingUpPanelLayout mLayout;
     private StorageReference mStorageRef;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,74 +46,88 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
         getSupportActionBar().hide(); //hide the title bar
         setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
+        String email = "vdrake05@gmail.com";
+        String password = "passpass1";
 
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            //Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toasty.success(MainActivity.this, "User Success!", Toast.LENGTH_SHORT, true).show();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            //Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            //Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                    //Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
         mStorageRef = FirebaseStorage.getInstance().getReference();
 
-        //For Demo Testing
-        ListView lv = findViewById(R.id.list);
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        final long ONE_MEGABYTE = 1024 * 1024 * 20;
+        StorageReference song1Ref = mStorageRef.child("Strobelights.mp3");
+        song1Ref.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this, "onItemClick", Toast.LENGTH_SHORT).show();
+            public void onSuccess(byte[] bytes) {
+                // Data for "songs/song1.mp3" is returns, use this as needed
+                MediaPlayer mediaPlayer =new MediaPlayer();
+
+                try {
+                    // create temp file that will hold byte array
+                    File tempMp3 = File.createTempFile("Strobelights", "mp3", getCacheDir());
+                    tempMp3.deleteOnExit();
+                    FileOutputStream fos = new FileOutputStream(tempMp3);
+                    fos.write(bytes);
+                    fos.close();
+
+                    // resetting mediaplayer instance to evade problems
+                    mediaPlayer.reset();
+
+                    // In case you run into issues with threading consider new instance like:
+                    // MediaPlayer mediaPlayer = new MediaPlayer();
+
+                    // Tried passing path directly, but kept getting
+                    // "Prepare failed.: status=0x1"
+                    // so using file descriptor instead
+                    FileInputStream fis = new FileInputStream(tempMp3);
+                    mediaPlayer.setDataSource(fis.getFD());
+
+                    mediaPlayer.prepare();
+                    //mediaPlayer.start();
+                    Toasty.success(MainActivity.this, "Success!", Toast.LENGTH_SHORT, true).show();
+                } catch (IOException ex) {
+                    String s = ex.toString();
+                    ex.printStackTrace();
+                    Toasty.error(MainActivity.this, "Error Playing File", Toast.LENGTH_SHORT, true).show();
+                }
+
             }
-        });
-        List<String> your_array_list = Arrays.asList(
-                "This",
-                "Is",
-                "An",
-                "Example",
-                "ListView",
-                "That",
-                "You",
-                "Can",
-                "Scroll",
-                ".",
-                "It",
-                "Shows",
-                "How",
-                "Any",
-                "Scrollable",
-                "View",
-                "Can",
-                "Be",
-                "Included",
-                "As",
-                "A",
-                "Child",
-                "Of",
-                "SlidingUpPanelLayout"
-        );
-
-        // This is the array adapter, it takes the context of the activity as a
-        // first parameter, the type of list view as a second parameter and your
-        // array as a third parameter.
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                your_array_list );
-
-        lv.setAdapter(arrayAdapter);
-        //End Demo Testing
-
-
-        mLayout = findViewById(R.id.sliding_layout);
-        mLayout.addPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
+        }).addOnFailureListener(new OnFailureListener() {
             @Override
-            public void onPanelSlide(View panel, float slideOffset) {
-                //when panel slides
-            }
-
-            @Override
-            public void onPanelStateChanged(View panel, SlidingUpPanelLayout.PanelState previousState, SlidingUpPanelLayout.PanelState newState) {
-                //when panel state changes
-            }
-        });
-
-        mLayout.setFadeOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mLayout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
             }
         });
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+
 }
